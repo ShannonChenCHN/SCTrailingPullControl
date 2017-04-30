2b13d41766421a854e5f3a5a1651d7626f233be6
ShannonChenCHN <273879688@qq.com>
2017-04-30 19:14:32 +0800

    Add framework and .travis.yml

---
+++ SCHorizontalPullControl/SCHorizontalPullControl.h
@@ -0,0 +1,20 @@
+//
+//  SCHorizontalPullControl.h
+//  SCHorizontalPullControl
+//
+//  Created by ShannonChen on 2017/4/30.
+//  Copyright © 2017年 ShannonChen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SCHorizontalPullControl.
+FOUNDATION_EXPORT double SCHorizontalPullControlVersionNumber;
+
+//! Project version string for SCHorizontalPullControl.
+FOUNDATION_EXPORT const unsigned char SCHorizontalPullControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SCHorizontalPullControl/PublicHeader.h>
+
+#import "SCTrailingPullControl.h"
+#import "UIScrollView+SCTrailingPullControl.h"
